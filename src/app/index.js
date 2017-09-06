bf606c7a7e7444addbaee91fbaa3b094b2181376
Benjamin M <benja.tush@gmail.com>
2017-09-07 00:15:05 +0300

    Using react components and props

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,22 @@
-alert('it woooorks!!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var createReactClass = require('create-react-class');
+
+// Create Component
+// var TodoComponent = createReactClass({
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <p><strong>Pizza name:</strong> {this.props.cheese.name}</p>
+                <p><strong>Pizza size:</strong> {this.props.cheese.size}</p>
+                <p><strong>Pizza price:</strong> {this.props.cheese.price}</p>
+            </div>
+        );
+    }
+});
+
+var Cheese = {name: 'Something Meaty', size: "Family size", price: "7.50"}
+
+// Put component into html page
+ReactDOM.render(< TodoComponent mssg= 'I like Pizza' cheese={Cheese} />, document.getElementById('todo-wrapper'));
